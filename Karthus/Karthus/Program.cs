e6fbffce30507fd8eb6df25329ae274134dbbf39
Xionanx <xionanx@hotmail.com>
2014-07-28 18:40:09 -0400

    Initial work on karthus

---
+++ Karthus/Karthus/Program.cs
@@ -0,0 +1,151 @@
+﻿#region
+
+using System.Collections.Generic;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System;
+using System.Linq;
+using System.Drawing;
+
+#endregion
+
+namespace Karthus
+{
+    class Program
+    {
+        public const string ChampionName = "Karthus";
+
+        //Orbwalker instance
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        //Spells
+        public static List<Spell> SpellList = new List<Spell>();
+
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+
+        //Menu
+        public static Menu Config;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad +=Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(System.EventArgs args)
+        {
+            if (ObjectManager.Player.BaseSkinName != ChampionName) return;
+
+            //Create the spells
+            Q = new Spell(SpellSlot.Q, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Q).SData.CastRangeDisplayOverride[0]);
+            W = new Spell(SpellSlot.W, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.CastRangeDisplayOverride[0]);
+            E = new Spell(SpellSlot.E, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.E).SData.CastRangeDisplayOverride[0]);
+            R = new Spell(SpellSlot.R, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).SData.CastRangeDisplayOverride[0]);
+
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+
+            //Create the menu
+            Config = new Menu(ChampionName, ChampionName, true);
+
+            //Orbwalker submenu
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+
+            //Load the orbwalker and add it to the submenu.
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+
+            Config.AddSubMenu(new Menu("Harass", "Harass"));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Use Q").SetValue(true));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseEHarass", "Use E").SetValue(true));
+            Config.SubMenu("Harass").AddItem(new MenuItem("HarassActive", "Harass!").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("Farm", "Farm"));
+            Config.SubMenu("Farm").AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(true));
+            Config.SubMenu("Farm").AddItem(new MenuItem("UseEFarm", "Use E").SetValue(true));
+            Config.SubMenu("Farm").AddItem(new MenuItem("LaneClearActive", "LaneClear!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("Auto Ult", "AutoUlt"));
+            Config.SubMenu("AutoUlt").AddItem(new MenuItem("AutoUlt", "Auto Ult for Kills").SetValue(true));
+
+            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+
+            Config.AddToMainMenu();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+         
+        }
+
+        private static void Game_OnGameUpdate(System.EventArgs args)
+        {
+            Orbwalker.SetAttacks(true);
+            if (Config.Item("AutoUlt").GetValue<bool>())
+            {
+                AutoULt();
+            }
+            
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+            else
+            {
+                if (Config.Item("HarassActive").GetValue<KeyBind>().Active)
+                    Harass();
+
+                if (Config.Item("LaneClearActive").GetValue<KeyBind>().Active)
+                    Farm();
+            }
+        }
+
+        private static void AutoULt()
+        {
+            var target = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+            if (DamageLib.getDmg(target, DamageLib.SpellType.R) > target.Health)
+            {
+                ObjectManager.Player.Spellbook.CastSpell(SpellSlot.R);
+            }
+        }
+
+        private static void Combo()
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private static void Farm()
+        {
+            // throw new System.NotImplementedException();
+        }
+
+        private static void Harass()
+        {
+            // throw new System.NotImplementedException();
+        }
+        
+        private static void Drawing_OnDraw(System.EventArgs args)
+        {
+            //Draw the ranges of the spells.
+            foreach (var spell in SpellList)
+            {
+                var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
+                if (menuItem.Active)
+                {
+                    Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
+                }
+            }
+        }
+    }
+}
