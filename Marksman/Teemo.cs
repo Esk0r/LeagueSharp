12aa616759cbcc7fdadce7590e5d921d16fe8c7a
BotHappy <Rigle00@gmail.com>
2014-08-09 00:15:55 +0200

    Added Teeto

---
+++ Marksman/Teemo.cs
@@ -0,0 +1,93 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Marksman
+{
+    internal class Teemo : Champion //Based on Tristana, modified by ~Ecko
+    {
+        public Spell Q;
+
+        public Teemo()
+        {
+            Utils.PrintMessage("Teemo loaded.");
+            
+            Q = new Spell(SpellSlot.Q, 580);
+        }
+
+        public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
+        {
+            if ((ComboActive || HarassActive) && unit.IsMe && (target is Obj_AI_Hero))
+            {
+                var useQ = GetValue<bool>("UseQ" + (ComboActive ? "C" : "H"));
+
+                if (useQ && Q.IsReady())
+                    Q.CastOnUnit(target);
+            }
+        }
+
+        public override void Drawing_OnDraw(EventArgs args)
+        {
+            Spell[] spellList = { Q};
+            foreach (var spell in spellList)
+            {
+                var menuItem = GetValue<Circle>("Draw" + spell.Slot);
+                if (menuItem.Active)
+                    Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
+            }
+        }
+
+        public override void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Orbwalking.CanMove(100) && (ComboActive || HarassActive))
+            {
+                var useQ = GetValue<bool>("UseQ" + (ComboActive ? "C" : "H"));
+                if (useQ)
+                {
+                    var qTarget = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
+                    if (Q.IsReady() && qTarget.IsValidTarget())
+                        Q.CastOnUnit(qTarget);
+                }
+            }
+
+            //Killsteal
+            if (!ComboActive || !GetValue<bool>("UseQM") || !Q.IsReady()) return;
+            foreach (
+                var hero in
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(
+                            hero =>
+                                hero.IsValidTarget(Q.Range) &&
+                                DamageLib.getDmg(hero, DamageLib.SpellType.Q) - 20 > hero.Health))
+                Q.CastOnUnit(hero);
+        }
+
+        public override void ComboMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseQC" + Id, "Use Q").SetValue(true));
+        }
+
+        public override void HarassMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseQH" + Id, "Use Q").SetValue(false));
+        }
+
+        public override void DrawingMenu(Menu config)
+        {
+            config.AddItem(
+                new MenuItem("DrawQ" + Id, "Q range").SetValue(new Circle(true, Color.FromArgb(100, 255, 0, 255))));
+
+        }
+
+        public override void MiscMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseQM" + Id, "Use Q").SetValue(true));
+        }
+    }
+}
