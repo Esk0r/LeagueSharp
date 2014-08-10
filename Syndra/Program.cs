eb8132c8dac7983174014235ba974b583052069a
Esk0r <kortatu1512@hotmail.com>
2014-08-10 14:38:06 +0200

    Revert "Fixed syndra ult calculations"
    This reverts commit 95626c5152847723fc456468be88dcb8cc5400e1.

--- Syndra/Program.cs
+++ Syndra/Program.cs
@@ -46,7 +46,7 @@ namespace Syndra
         {
             Player = ObjectManager.Player;
 
-            if (Player.ChampionName != ChampionName) return;
+            if (Player.BaseSkinName != ChampionName) return;
 
             //Create the spells
             Q = new Spell(SpellSlot.Q, 790);
@@ -181,7 +181,7 @@ namespace Syndra
         private static void Interrupter_OnPosibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
         {
             if (!Config.Item("InterruptSpells").GetValue<bool>()) return;
-
+          
             if (Player.Distance(unit) < E.Range && E.IsReady())
             {
                 Q.Cast(unit.ServerPosition);
@@ -268,20 +268,13 @@ namespace Syndra
                 damage += DamageLib.getDmg(enemy, DamageLib.SpellType.E);
 
             var igniteSlot = Player.GetSpellSlot("SummonerDot");
-
-            if (igniteSlot != SpellSlot.Unknown && Player.SummonerSpellbook.CanUseSpell(igniteSlot) == SpellState.Ready)
+            
+            if(igniteSlot != SpellSlot.Unknown && Player.SummonerSpellbook.CanUseSpell(igniteSlot) == SpellState.Ready)
                 damage += DamageLib.getDmg(enemy, DamageLib.SpellType.IGNITE);
+                
 
             if (R.IsReady())
-            {
-                var orbCount = Player.Spellbook.GetSpell(SpellSlot.R).Ammo;
-                var defaultDamage = orbCount > 3
-                    ? orbCount * DamageLib.getDmg(enemy, DamageLib.SpellType.R)
-                    : DamageLib.getDmg(enemy, DamageLib.SpellType.R, DamageLib.StageType.FirstDamage);
-                var maxDamage = DamageLib.getDmg(enemy, DamageLib.SpellType.R, DamageLib.StageType.SecondDamage);
-
-                damage += defaultDamage <= maxDamage ? defaultDamage : maxDamage;
-            }
+                damage += Player.Spellbook.GetSpell(SpellSlot.R).Ammo * DamageLib.getDmg(enemy, DamageLib.SpellType.R);
 
             return (float)damage * (DFG.IsReady() ? 1.2f : 1);
         }
@@ -337,10 +330,11 @@ namespace Syndra
             if (rTarget != null && useR && GetComboDamage(rTarget) > rTarget.Health && DFG.IsReady())
             {
                 DFG.Cast(rTarget);
-                if (R.IsReady())
-                {
+                if(R.IsReady())
+                {       
                     R.Cast(rTarget);
                 }
+                
             }
 
             //R
@@ -414,15 +408,15 @@ namespace Syndra
                     var fl1 = Q.GetCircularFarmLocation(rangedMinionsQ, Q.Width);
                     var fl2 = Q.GetCircularFarmLocation(allMinionsQ, Q.Width);
 
-                    if (fl1.MinionsHit >= 3)
-                    {
-                        Q.Cast(fl1.Position);
-                    }
+                        if (fl1.MinionsHit >= 3)
+                        {
+                            Q.Cast(fl1.Position);
+                        }
 
-                    else if (fl2.MinionsHit >= 2 || allMinionsQ.Count == 1)
-                    {
-                        Q.Cast(fl2.Position);
-                    }
+                        else if (fl2.MinionsHit >= 2 || allMinionsQ.Count == 1)
+                        {
+                            Q.Cast(fl2.Position);
+                        }
                 }
                 else
                     foreach (var minion in allMinionsQ)
