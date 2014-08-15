0340b181b7bed797090507f65cee42dd69c58f01
pqmailer <pqmailer@gmail.com>
2014-08-15 18:27:30 +0200

    fixed cait

--- Marksman/Caitlyn.cs
+++ Marksman/Caitlyn.cs
@@ -7,7 +7,8 @@ using LeagueSharp.Common;
 
 #endregion
 
-namespace Marksman {
+namespace Marksman
+{
     internal class Caitlyn : Champion // Base done by Chulbul-Pandey, Drawings and improvements added by Dibes.
     {
         public Spell E;
@@ -17,29 +18,31 @@ namespace Marksman {
         public bool ShowUlt;
         public string UltTarget;
 
-        public Caitlyn() {
+        public Caitlyn()
+        {
             Utils.PrintMessage("Caitlyn loaded.");
 
             Q = new Spell(SpellSlot.Q, 1240);
-            Q.SetSkillshot(0.25f, 60f, 2000f, false, Prediction.SkillshotType.SkillshotLine);
-
             E = new Spell(SpellSlot.E, 800);
-            E.SetSkillshot(0.25f, 80f, 1600f, true, Prediction.SkillshotType.SkillshotLine);
-
             R = new Spell(SpellSlot.R, 2000);
 
+            Q.SetSkillshot(0.25f, 60f, 2000f, false, Prediction.SkillshotType.SkillshotLine);
+            E.SetSkillshot(0.25f, 80f, 1600f, true, Prediction.SkillshotType.SkillshotLine);
+
             AntiGapcloser.OnEnemyGapcloser += AntiGapcloser_OnEnemyGapcloser;
-            Interrupter.OnPosibleToInterrupt += Interrupter_OnPosibleToInterrupt;
         }
 
-        public void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser) {
+        public void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
             if (E.IsReady() && gapcloser.Sender.IsValidTarget(E.Range))
                 E.CastOnUnit(gapcloser.Sender);
         }
 
-        public override void Drawing_OnDraw(EventArgs args) {
+        public override void Drawing_OnDraw(EventArgs args)
+        {
             Spell[] spellList = { Q, E, R };
-            foreach (var spell in spellList) {
+            foreach (var spell in spellList)
+            {
                 var menuItem = GetValue<Circle>("Draw" + spell.Slot);
                 if (menuItem.Active)
                     Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
@@ -47,35 +50,45 @@ namespace Marksman {
 
             var drawUlt = GetValue<Circle>("DrawUlt");
 
-            if (drawUlt.Active && ShowUlt) {
+            if (drawUlt.Active && ShowUlt)
+            {
                 var playerPos = Drawing.WorldToScreen(ObjectManager.Player.Position);
                 Drawing.DrawText(playerPos[0] - 65, playerPos[1] + 20, drawUlt.Color, "Hit R To kill " + UltTarget + "!");
             }
         }
 
-        public override void Game_OnGameUpdate(EventArgs args) {
+        public override void Game_OnGameUpdate(EventArgs args)
+        {
             R.Range = 500 * R.Level + 1500;
 
             Obj_AI_Hero vTarget;
 
-            if (R.IsReady()) {
+            if (R.IsReady())
+            {
                 vTarget = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Physical);
-                if (vTarget.IsValidTarget(R.Range) && vTarget.Health <= R.GetDamage(vTarget)) {
+                if (vTarget.IsValidTarget(R.Range) && vTarget.Health <= R.GetDamage(vTarget))
+                {
                     if (GetValue<KeyBind>("UltHelp").Active)
                         R.Cast(vTarget);
 
                     UltTarget = vTarget.ChampionName;
                     ShowUlt = true;
-                } else {
+                }
+                else
+                {
                     ShowUlt = false;
                 }
-            } else {
+            }
+            else
+            {
                 ShowUlt = false;
             }
 
-            if (GetValue<KeyBind>("UseEQC").Active && E.IsReady() && Q.IsReady()) {
+            if (GetValue<KeyBind>("UseEQC").Active && E.IsReady() && Q.IsReady())
+            {
                 vTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
-                if (vTarget.IsValidTarget(E.Range)) {
+                if (vTarget.IsValidTarget(E.Range))
+                {
                     E.Cast(vTarget);
                     Q.Cast(vTarget, false, true);
                 }
@@ -87,26 +100,32 @@ namespace Marksman {
             var useE = GetValue<bool>("UseEC");
             var useR = GetValue<bool>("UseRC");
 
-            if (Q.IsReady() && useQ) {
+            if (Q.IsReady() && useQ)
+            {
                 vTarget = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
                 if (vTarget != null)
                     Q.Cast(vTarget, false, true);
-            } else if (E.IsReady() && useE) {
+            }
+            else if (E.IsReady() && useE)
+            {
                 vTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
                 if (vTarget != null && vTarget.Health <= E.GetDamage(vTarget))
                     E.Cast(vTarget);
             }
 
-            if (R.IsReady() && useR) {
+            if (R.IsReady() && useR)
+            {
                 vTarget = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Physical);
                 if (vTarget != null && vTarget.Health <= R.GetDamage(vTarget) &&
-                    !Orbwalking.InAutoAttackRange(vTarget)) {
+                    !Orbwalking.InAutoAttackRange(vTarget))
+                {
                     R.CastOnUnit(vTarget);
                 }
             }
         }
 
-        public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target) {
+        public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
+        {
             if ((!ComboActive && !HarassActive) || unit.IsMe || (!(target is Obj_AI_Hero))) return;
 
             var useQ = GetValue<bool>("UseQ" + (ComboActive ? "C" : "H"));
@@ -114,17 +133,20 @@ namespace Marksman {
                 Q.Cast(target, false, true);
         }
 
-        public override void ComboMenu(Menu config) {
+        public override void ComboMenu(Menu config)
+        {
             config.AddItem(new MenuItem("UseQC" + Id, "Use Q").SetValue(true));
             config.AddItem(new MenuItem("UseEC" + Id, "Use E").SetValue(true));
             config.AddItem(new MenuItem("UseRC" + Id, "Use R").SetValue(true));
         }
 
-        public override void HarassMenu(Menu config) {
+        public override void HarassMenu(Menu config)
+        {
             config.AddItem(new MenuItem("UseQH" + Id, "Use Q").SetValue(true));
         }
 
-        public override void DrawingMenu(Menu config) {
+        public override void DrawingMenu(Menu config)
+        {
             config.AddItem(
                 new MenuItem("DrawQ" + Id, "Q range").SetValue(new Circle(true, Color.FromArgb(100, 255, 0, 255))));
             config.AddItem(
@@ -135,7 +157,8 @@ namespace Marksman {
                 new MenuItem("DrawUlt" + Id, "Ult Text").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
         }
 
-        public override void MiscMenu(Menu config) {
+        public override void MiscMenu(Menu config)
+        {
             config.AddItem(
                 new MenuItem("UltHelp" + Id, "Ult Target on R").SetValue(new KeyBind("R".ToCharArray()[0],
                     KeyBindType.Press)));
