f94c6ba2f33f0f397c9ee538f259bd1e67f17b1b
andreluis034 <andrebrandao@sapo.pt>
2014-09-12 15:04:38 +0100

    Resharp

--- Marksman/Vayne.cs
+++ Marksman/Vayne.cs
@@ -1,9 +1,6 @@
 ﻿#region
 
 using System;
-using System.Drawing;
-using System.Linq;
-using System.Security.AccessControl;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -17,6 +14,7 @@ namespace Marksman
         public Spell E;
         public Spell Q;
         public Spell QE;
+
         public Vayne()
         {
             Utils.PrintMessage("Vayne loaded");
@@ -35,13 +33,17 @@ namespace Marksman
         public void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
         {
             if (E.IsReady() && gapcloser.Sender.IsValidTarget(E.Range))
+            {
                 E.CastOnUnit(gapcloser.Sender);
+            }
         }
 
         public void Interrupter_OnPosibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
         {
             if (GetValue<bool>("UseEInterrupt") && unit.IsValidTarget(550f))
+            {
                 E.Cast(unit);
+            }
         }
 
 
@@ -49,16 +51,19 @@ namespace Marksman
         {
             if ((!E.IsReady()) ||
                 ((Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.Combo || !GetValue<bool>("UseEC")) &&
-                 !GetValue<KeyBind>("UseET").Active)) return;
+                 !GetValue<KeyBind>("UseET").Active))
+            {
+                return;
+            }
 
             var normalTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
-            var qeTarget = SimpleTs.GetTarget(200+E.Range, SimpleTs.DamageType.Physical);
+            var qeTarget = SimpleTs.GetTarget(200 + E.Range, SimpleTs.DamageType.Physical);
             if (normalTarget.IsValid)
             {
                 if (IsStunable(normalTarget))
                 {
                     E.Cast(normalTarget);
-                }   
+                }
             }
             else if (qeTarget.IsValid)
             {
@@ -69,49 +74,51 @@ namespace Marksman
                 Q.Cast(qeTarget);
                 Utility.DelayAction.Add(250, () => E.Cast(qeTarget));
             }
-
-
         }
-
-
 
 
         public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
         {
             if (unit.IsMe && Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && GetValue<bool>("UseQC"))
+            {
                 CastTumble((Obj_AI_Hero) target);
+            }
         }
 
         public bool IsStunable(Obj_AI_Hero enemy)
         {
-            var prediction = Vector3.DistanceSquared(enemy.ServerPosition, ObjectManager.Player.ServerPosition) > 550*550 ? QE.GetPrediction(enemy) : E.GetPrediction(enemy);
-            
+            var prediction =
+                Vector3.DistanceSquared(enemy.ServerPosition, ObjectManager.Player.ServerPosition) > 550 * 550
+                    ? QE.GetPrediction(enemy)
+                    : E.GetPrediction(enemy);
+
             if (prediction.Hitchance.Equals(HitChance.OutOfRange))
             {
                 return false;
             }
 
-            for (var i = 0; i < GetValue<Slider>("PushDistance").Value; i += (int)enemy.BoundingRadius)
+            for (var i = 0; i < GetValue<Slider>("PushDistance").Value; i += (int) enemy.BoundingRadius)
             {
-                if (NavMesh.GetCollisionFlags(prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D()).HasFlag(CollisionFlags.Wall))
+                if (
+                    NavMesh.GetCollisionFlags(
+                        prediction.UnitPosition.To2D().Extend(ObjectManager.Player.ServerPosition.To2D(), -i).To3D())
+                        .HasFlag(CollisionFlags.Wall))
                 {
                     return true;
                 }
-                
             }
             return false;
         }
 
         public void CastTumble(Obj_AI_Hero target)
         {
-
-            var posAfterTumble = ObjectManager.Player.ServerPosition.To2D().Extend(Game.CursorPos.To2D(), 300).To3D();
+            var posAfterTumble =
+                ObjectManager.Player.ServerPosition.To2D().Extend(Game.CursorPos.To2D(), 300).To3D();
             var distanceAfterTumble = Vector3.DistanceSquared(posAfterTumble, target.ServerPosition);
-            if (distanceAfterTumble < 550*550 && distanceAfterTumble > 100*100)
+            if (distanceAfterTumble < 550 * 550 && distanceAfterTumble > 100 * 100)
             {
                 Q.Cast(Game.CursorPos);
             }
-
         }
 
         public override void ComboMenu(Menu config)
@@ -127,8 +134,7 @@ namespace Marksman
                 new MenuItem("UseET" + Id, "Use E (Toggle)").SetValue(
                     new KeyBind("T".ToCharArray()[0], KeyBindType.Toggle)));
             config.AddItem(new MenuItem("UseEInterrupt" + Id, "Use E To Interrupt").SetValue(true));
-            config.AddItem(
-                new MenuItem("PushDistance" + Id, "E Push Distance").SetValue(new Slider(425, 475, 300)));
+            config.AddItem(new MenuItem("PushDistance" + Id, "E Push Distance").SetValue(new Slider(425, 475, 300)));
         }
     }
 }
