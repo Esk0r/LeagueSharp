d7e5e4109e39ba1d35fbb4bdd617605f8b360996
xQx <xQxCPMxQx@gmail.com>
2014-10-18 12:59:47 +0300

    Update Varus.cs

--- Marksman/Varus.cs
+++ Marksman/Varus.cs
@@ -157,10 +157,10 @@ namespace Marksman
                 //     return;
                 //var wBuff = qTarget.Buffs.Count(buff => buff.Name == "varuswdebuff");
                 
-               // if (qTarget.Health < CalcQDamage(qTarget) + CalcWExplodeDamage(qTarget))
-               /*     CastQEnemy(qTarget);
+                if (qTarget.Health < CalcQDamage(qTarget) + CalcWExplodeDamage(qTarget))
+                     CastQEnemy(qTarget);
                 else
-                {*/
+                {
                     switch (useQ.SelectedIndex)
                     {
                         case 1: /* [ Use Q everytime ] */
@@ -181,7 +181,7 @@ namespace Marksman
                                 break;
                             }
                     }
-                //}
+                }
                 
                 if (useE && E.IsReady() && eTarget != null)
                     E.Cast(eTarget, false, true);
