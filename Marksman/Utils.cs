3f699d8333f4ffed71ab69db196c4d6a0f0ae685
xQx <xQxCPMxQx@gmail.com>
2014-11-19 13:30:07 +0200

    Added recommend Evade notice

--- Marksman/Utils.cs
+++ Marksman/Utils.cs
@@ -1,5 +1,6 @@
-﻿#region
+#region
 
+using System.Linq;
 using LeagueSharp;
 
 #endregion
@@ -8,9 +9,23 @@ namespace Marksman
 {
     internal static class Utils
     {
+        private static readonly string[] BetterWithEvade =
+        {
+            "Corki", "Ezreal", "Graves", "Lucian", "Sivir", "Tristana", "Caitlyn", "Vayne"
+        };
+
         public static void PrintMessage(string message)
         {
             Game.PrintChat("<font color='#70DBDB'>Marksman:</font> <font color='#FFFFFF'>" + message + "</font>");
+            
+            foreach (
+                var xChampList in BetterWithEvade.Where(xChampList => ObjectManager.Player.ChampionName == xChampList))
+            {
+                Game.PrintChat(
+                    string.Format(
+                        "<font color='#70DBDB'>We recommend you use Evade# with </font>{0}<font color='#70DBDB'> http://joduska.me/forum/topic/26-",
+                        xChampList));
+            }
         }
     }
 }
