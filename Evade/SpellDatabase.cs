1c7e454b1e333cf274496c9e22f13423b6ef7a07
Esk0r <kortatu1512@hotmail.com>
2014-11-21 00:32:35 +0100

    Revert "Added Kalista's Q"
    This reverts commit 2c15eafc5fdf23b995a32eacf849876f2482cc67.

--- Evade/SpellDatabase.cs
+++ Evade/SpellDatabase.cs
@@ -1400,32 +1400,6 @@ namespace Evade
 
             #endregion Jinx
 
-            #region Kalista
-
-            Spells.Add(
-                new SpellData
-                {
-                    ChampionName = "Kalista",
-                    SpellName = "KalistaMysticShot",
-                    Slot = SpellSlot.Q,
-                    Type = SkillShotType.SkillshotMissileLine,
-                    Delay = 250,
-                    Range = 1200,
-                    Radius = 40,
-                    MissileSpeed = 1700,
-                    FixedRange = true,
-                    AddHitbox = true,
-                    DangerValue = 2,
-                    IsDangerous = false,
-                    MissileSpellName = "kalistamysticshotmis",
-                    ExtraMissileNames = new[] { "kalistamysticshotmistrue" },
-                    CanBeRemoved = true,
-                    CollisionObjects =
-                        new[] { CollisionObjectTypes.Champions, CollisionObjectTypes.Minion, CollisionObjectTypes.YasuoWall },
-                });
-
-            #endregion Kalista
-
             #region Karma
 
             Spells.Add(
