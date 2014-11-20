e34c86417c1301874645a37767910d725b970431
xQx <xQxCPMxQx@gmail.com>
2014-11-20 10:23:41 +0200

    Update MissFortune.cs

--- Marksman/MissFortune.cs
+++ Marksman/MissFortune.cs
@@ -53,20 +53,48 @@ namespace Marksman
             }
         }
 
+        private static void CastQ()
+        {
+            if (!Q.IsReady())
+                return;
+
+            var t = SimpleTs.GetTarget(Q.Range + 450, SimpleTs.DamageType.Physical);
+            if (t.IsValidTarget(Q.Range))
+            {
+                Q.CastOnUnit(t);
+                return;
+            }
+
+            var vMinions = MinionManager.GetMinions(t.Position, Q.Range, MinionTypes.All, MinionTeam.NotAlly);
+            Obj_AI_Base[] nearstMinion = { null };
+            foreach (
+                var vMinion in
+                    vMinions.Where(
+                        minion =>
+                            minion.Distance(ObjectManager.Player) <= t.Distance(ObjectManager.Player) &&
+                            t.Distance(minion) < 400)
+                        .Where(
+                            minion =>
+                                nearstMinion[0] == null ||
+                                minion.Distance(ObjectManager.Player) < nearstMinion[0].Distance(ObjectManager.Player)))
+                
+                nearstMinion[0] = vMinion;
+            if (nearstMinion[0] != null && nearstMinion[0].IsValidTarget(Q.Range))
+                Q.CastOnUnit(nearstMinion[0]);
+        }
+
         public override void Game_OnGameUpdate(EventArgs args)
         {
             if (Q.IsReady() && GetValue<KeyBind>("UseQTH").Active)
             {
-                if(ObjectManager.Player.HasBuff("Recall"))
+                if (ObjectManager.Player.HasBuff("Recall"))
                     return;
-                var t = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
-                if (t != null)
-                    Q.Cast(t);
+                CastQ();
             }
-                
+
             if (E.IsReady() && GetValue<KeyBind>("UseETH").Active)
             {
-                if(ObjectManager.Player.HasBuff("Recall"))
+                if (ObjectManager.Player.HasBuff("Recall"))
                     return;
                 var t = Orbwalker.GetTarget() ??
                         SimpleTs.GetTarget(E.Range + E.Range / 2, SimpleTs.DamageType.Physical);
@@ -84,12 +112,7 @@ namespace Marksman
                 {
                     if (Q.IsReady() && useQ)
                     {
-                        var vTarget = Orbwalker.GetTarget() ?? 
-                                      SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
-                        if (vTarget != null)
-                        {
-                            Q.CastOnUnit(vTarget);
-                        }
+                        CastQ();
                     }
 
                     if (E.IsReady() && useE)
@@ -119,11 +142,11 @@ namespace Marksman
             config.AddItem(new MenuItem("UseEH" + Id, "Use E").SetValue(true));
             config.AddItem(
                 new MenuItem("UseQTH" + Id, "Use Q (Toggle)").SetValue(new KeyBind("H".ToCharArray()[0],
-                    KeyBindType.Toggle))); 
+                    KeyBindType.Toggle)));
             config.AddItem(
                 new MenuItem("UseETH" + Id, "Use E (Toggle)").SetValue(new KeyBind("T".ToCharArray()[0],
-                    KeyBindType.Toggle))); 
-                    
+                    KeyBindType.Toggle)));
+
             return true;
         }
 
