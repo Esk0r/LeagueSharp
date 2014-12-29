f4a0685ff3774a896c9d6c16be23e7567b5b922a
xQx <xQxCPMxQx@gmail.com>
2014-12-29 20:37:05 +0200

    Update Utils.cs

--- Marksman/Utils.cs
+++ Marksman/Utils.cs
@@ -2,7 +2,9 @@
 
 using System.Linq;
 using LeagueSharp;
-
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
 #endregion
 
 namespace Marksman
@@ -27,5 +29,32 @@ namespace Marksman
                         xChampList));
             }
         }
+
+        internal static class Jungle
+        {
+            public static void DrawJunglePosition()
+            {
+                if (Game.MapId == (GameMapId) 11)
+                {
+                    const float circleRange = 75f;
+
+                    Utility.DrawCircle(new Vector3(7461.018f, 3253.575f, 52.57141f), circleRange, Color.Blue, 1, 15); // blue team :red
+                    Utility.DrawCircle(new Vector3(3511.601f, 8745.617f, 52.57141f), circleRange, Color.Blue, 1, 15); // blue team :blue
+                    Utility.DrawCircle(new Vector3(7462.053f, 2489.813f, 52.57141f), circleRange, Color.Blue, 1, 15); // blue team :golems
+                    Utility.DrawCircle(new Vector3(3144.897f, 7106.449f, 51.89026f), circleRange, Color.Blue, 1, 15); // blue team :wolfs
+                    Utility.DrawCircle(new Vector3(7770.341f, 5061.238f, 49.26587f), circleRange, Color.Blue, 1, 15); // blue team :wariaths
+
+                    Utility.DrawCircle(new Vector3(10930.93f, 5405.83f, -68.72192f), circleRange, Color.Yellow, 1, 15); // Dragon
+
+                    Utility.DrawCircle(new Vector3(7326.056f, 11643.01f, 50.21985f), circleRange, Color.Red, 1, 15); // red team :red
+                    Utility.DrawCircle(new Vector3(11417.6f, 6216.028f, 51.00244f), circleRange, Color.Red, 1, 15); // red team :blue
+                    Utility.DrawCircle(new Vector3(7368.408f, 12488.37f, 56.47668f), circleRange, Color.Red, 1, 15); // red team :golems
+                    Utility.DrawCircle(new Vector3(10342.77f, 8896.083f, 51.72742f), circleRange, Color.Red, 1, 15); // red team :wolfs
+                    Utility.DrawCircle(new Vector3(7001.741f, 9915.717f, 54.02466f), circleRange, Color.Red, 1, 15); // red team :wariaths                    
+                }                
+            }
+        }
     }
+    
+    
 }
