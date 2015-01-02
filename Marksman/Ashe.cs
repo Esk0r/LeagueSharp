2785508e40564d5883773149d94de3b989bef1c4
xQx <xQxCPMxQx@gmail.com>
2015-01-02 18:26:19 +0200

    Update

--- Marksman/Ashe.cs
+++ Marksman/Ashe.cs
@@ -1,16 +1,13 @@
 #region
-
 using System;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using Color = System.Drawing.Color;
-
 #endregion
 
 namespace Marksman
 {
-
     internal class Ashe : Champion
     {
         public static Spell Q;
@@ -18,19 +15,20 @@ namespace Marksman
         public static Spell E;
         public static Spell R;
 
-        public static bool QActive = false;
-
         public Ashe()
         {
             Q = new Spell(SpellSlot.Q);
             W = new Spell(SpellSlot.W, 1200);
             E = new Spell(SpellSlot.E, 2500);
             R = new Spell(SpellSlot.R, 20000);
-            W.SetSkillshot(250f, (float)(24.32f * Math.PI / 180), 902f, true, SkillshotType.SkillshotCone);
+
+            W.SetSkillshot(250f, (float) (24.32f*Math.PI/180), 902f, true, SkillshotType.SkillshotCone);
             E.SetSkillshot(377f, 299f, 1400f, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(250f, 130f, 1600f, false, SkillshotType.SkillshotLine);
+            
             Interrupter.OnPossibleToInterrupt += Game_OnPossibleToInterrupt;
             Obj_AI_Base.OnProcessSpellCast += Game_OnProcessSpell;
+            
             Utils.PrintMessage("Ashe loaded.");
         }
 
@@ -44,44 +42,15 @@ namespace Marksman
 
         public void Game_OnProcessSpell(Obj_AI_Base unit, GameObjectProcessSpellCastEventArgs spell)
         {
-            if (unit.IsMe)
-            {
-                if (spell.SData.Name.ToLower() == "frostshot")
-                    QActive = !QActive;
-
-                if (spell.SData.Name.ToLower() == "frostarrow")
-                {
-                    if (LaneClearActive && Config.Item("DeactivateQ" + Id).GetValue<bool>()) return;
-
-                    Q.Cast();
-                }
-            }
-
-            if (!Config.Item("EFlash" + Id).GetValue<bool>() || unit.Team == ObjectManager.Player.Team) return;
+            if (!Config.Item("EFlash" + Id).GetValue<bool>() || unit.Team == ObjectManager.Player.Team) 
+                return;
 
             if (spell.SData.Name.ToLower() == "summonerflash")
                 E.Cast(spell.End);
         }
 
-        public override void Drawing_OnDraw(EventArgs args)
-        {
-            var drawW = Config.Item("DrawW" + Id).GetValue<Circle>();
-            if (drawW.Active)
-            {
-                Utility.DrawCircle(ObjectManager.Player.Position, W.Range, drawW.Color);
-            }
-
-            var drawE = Config.Item("DrawE" + Id).GetValue<Circle>();
-            if (drawE.Active)
-            {
-                Utility.DrawCircle(ObjectManager.Player.Position, E.Range, drawE.Color);
-            }
-
-        }
-
         public override void Game_OnGameUpdate(EventArgs args)
         {
-            
             if (W.IsReady() && GetValue<KeyBind>("UseWTH").Active)
             {
                 if(ObjectManager.Player.HasBuff("Recall"))
@@ -96,7 +65,7 @@ namespace Marksman
                 var target = TargetSelector.GetTarget(1200, TargetSelector.DamageType.Physical);
                 if (target == null) return;
 
-                if (!Config.Item("QExploit" + Id).GetValue<bool>() && !IsQActive() && Config.Item("UseQC" + Id).GetValue<bool>())
+                if (!IsQActive && Config.Item("UseQC" + Id).GetValue<bool>())
                     Q.Cast();
 
                 if (Config.Item("UseWC" + Id).GetValue<bool>() && W.IsReady())
@@ -119,16 +88,12 @@ namespace Marksman
                 var target = TargetSelector.GetTarget(1200, TargetSelector.DamageType.Physical);
                 if (target == null) return;
 
-                if (!Config.Item("QExploit" + Id).GetValue<bool>() && !IsQActive() && Config.Item("UseQH" + Id).GetValue<bool>())
+                if (!IsQActive && Config.Item("UseQH" + Id).GetValue<bool>())
                     Q.Cast();
 
                 if (Config.Item("UseWH" + Id).GetValue<bool>() && W.IsReady())
                     W.Cast(target);
             }
-
-            //Lane Clear
-            if (LaneClearActive && Config.Item("DeactivateQ" + Id).GetValue<bool>() && IsQActive())
-                Q.Cast();
 
             //Manual cast R
             if (Config.Item("RManualCast" + Id).GetValue<KeyBind>().Active)
@@ -136,14 +101,6 @@ namespace Marksman
                 var rTarget = TargetSelector.GetTarget(2000, TargetSelector.DamageType.Physical);
                 R.Cast(rTarget);
             }
-        }
-
-        public override void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
-        {
-            if (LaneClearActive && Config.Item("DeactivateQ" + Id).GetValue<bool>()) return;
-
-            if ((Config.Item("QExploit" + Id).GetValue<bool>() && !IsQActive()))
-                Q.Cast();
         }
 
         public override bool ComboMenu(Menu config)
@@ -166,7 +123,7 @@ namespace Marksman
 
         public override bool LaneClearMenu(Menu config)
         {
-            config.AddItem(new MenuItem("DeactivateQ" + Id, "Always deactivate Frost Arrow").SetValue(false));
+
             return true;
         }
 
@@ -181,7 +138,6 @@ namespace Marksman
 
         public override bool MiscMenu(Menu config)
         {
-            config.AddItem(new MenuItem("QExploit" + Id, "Use Q Exploit").SetValue(true));
             config.AddItem(new MenuItem("RInterruptable" + Id, "Auto R Interruptable Spells").SetValue(true));
             config.AddItem(new MenuItem("EFlash" + Id, "Use E against Flashes").SetValue(true));
             config.AddItem(new MenuItem("RManualCast" + Id, "Cast R Manually(2000 range)"))
@@ -189,14 +145,28 @@ namespace Marksman
             return true;
         }
 
-        public static bool IsQActive()
+        public bool IsQActive
         {
-            if (ObjectManager.Player.HasBuff("FrostShot"))
-                QActive = true;
-
-            return QActive;
+            get
+            {
+                return ObjectManager.Player.HasBuff("FrostShot");
+            }
         }
 
+        public override void Drawing_OnDraw(EventArgs args)
+        {
+            var drawW = Config.Item("DrawW" + Id).GetValue<Circle>();
+            if (drawW.Active)
+            {
+                Utility.DrawCircle(ObjectManager.Player.Position, W.Range, drawW.Color);
+            }
+
+            var drawE = Config.Item("DrawE" + Id).GetValue<Circle>();
+            if (drawE.Active)
+            {
+                Utility.DrawCircle(ObjectManager.Player.Position, E.Range, drawE.Color);
+            }
+        }
         public override bool ExtrasMenu(Menu config)
         {
 
