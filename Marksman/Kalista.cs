af6e8585af1f76d222e72b64014820d127725cb5
xQx <xQxCPMxQx@gmail.com>
2015-01-14 15:37:32 +0200

    Update Kalista.cs

--- Marksman/Kalista.cs
+++ Marksman/Kalista.cs
@@ -41,12 +41,12 @@ namespace Marksman
         {
             Utils.PrintMessage("Kalista loaded.");
 
-            Q = new Spell(SpellSlot.Q, 1300);
+            Q = new Spell(SpellSlot.Q, 1150);
             W = new Spell(SpellSlot.W, 5500);
             E = new Spell(SpellSlot.E, 950);
             R = new Spell(SpellSlot.R, 1250);
 
-            Q.SetSkillshot(0.25f, 60f, 2000f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 40f, 1300f, true, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.25f, 80f, 1600f, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(1f, 160f, 2000f, false, SkillshotType.SkillshotLine);
         }
