1f6fe0448a6f0fb13f6cddddac1bc9752f82ab25
xQx <xQxCPMxQx@gmail.com>
2015-01-23 21:16:56 +0200

    Updated Combo Damage

--- Marksman/Ezreal.cs
+++ Marksman/Ezreal.cs
@@ -12,6 +12,7 @@ namespace Marksman
     internal class Ezreal : Champion
     {
         public static Spell Q;
+        public static Spell E;
         public static Spell W;
         public static Spell R;
         public static Font vText;
@@ -26,6 +27,8 @@ namespace Marksman
             W = new Spell(SpellSlot.W, 800);
             W.SetSkillshot(0.25f, 80f, 1600f, false, SkillshotType.SkillshotLine);
 
+            E = new Spell(SpellSlot.E);
+            
             R = new Spell(SpellSlot.R, 2500);
             R.SetSkillshot(1f, 160f, 2000f, false, SkillshotType.SkillshotLine);
 
@@ -200,6 +203,9 @@ namespace Marksman
 
             if (W.IsReady())
                 fComboDamage += (float) ObjectManager.Player.GetSpellDamage(t, SpellSlot.W);
+
+            if (E.IsReady())
+                fComboDamage += (float) ObjectManager.Player.GetSpellDamage(t, SpellSlot.E);
 
             if (R.IsReady())
                 fComboDamage += (float) ObjectManager.Player.GetSpellDamage(t, SpellSlot.R);
