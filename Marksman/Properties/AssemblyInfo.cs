70b8d70f6a5740462a9e0b5748c7822b827eff5d
xQx <xQxCPMxQx@gmail.com>
2015-02-08 23:20:49 +0200

    Update AssemblyInfo.cs

--- Marksman/Properties/AssemblyInfo.cs
+++ Marksman/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [ assembly: AssemblyConfiguration("") ]
 [ assembly: AssemblyCompany("") ]
 [ assembly: AssemblyProduct("Marksman") ]
-[ assembly: AssemblyCopyright("Copyright ©  2014") ]
+[ assembly: AssemblyCopyright("Copyright ©  2015") ]
 [ assembly: AssemblyTrademark("") ]
 [ assembly: AssemblyCulture("") ]
 
