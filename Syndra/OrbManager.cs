abbd4624466ea90cb021b9ecc587714f7fd587ef
Esk0r <kortatu1512@hotmail.com>
2015-04-29 13:28:33 +0200

    Updated

--- Syndra/OrbManager.cs
+++ Syndra/OrbManager.cs
@@ -67,7 +67,7 @@ namespace Syndra
 
         private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
-            if (args.PacketData[0] == 0x24)
+            if (args.PacketData[0] == 0xBA)
             {
                 var packet = new GamePacket(args.PacketData);
                 packet.Position = 2;
